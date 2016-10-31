10fb7e3a02f454e57990c30a0ac1a176d99183fc
liuhongjiang <andrew.lhj@gmail.com>
2016-10-30 23:14:51 -0400

    Added push after the build

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ sudo pip install .
 sudo mkdir /etc/kolla/
 sudo cp /home/vagrant/kolla-config/* /etc/kolla/
 
-kolla-build --config-dir /home/vagrant/kolla-config
+kolla-build --config-dir /home/vagrant/kolla-config --push
 
 kolla-ansible deploy --configdir /home/vagrant/kolla-config -i /home/vagrant/kolla-config/multinode -vvv
 
